f8fcc9ccb60ce8274ba00d23ed5d6a8c6a0da5cb
disha748-design <110846789+disha748-design@users.noreply.github.com>
2025-09-04 01:11:26 +0530

    Ignore Visual Studio cache files

--- backend/obj/Debug/net8.0/BlogApi.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/BlogApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlogApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55fd23982d9814add01e48ebdc6b519be5317f4b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26d4a298d9ed24ea1655da79b9912f5c808e334d")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlogApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlogApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
